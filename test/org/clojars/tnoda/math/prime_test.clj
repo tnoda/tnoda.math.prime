8a2ba10d47ea9a32f681b6a1fbc847ce586afe61
Takahiro Noda <takahiro.noda@gmail.com>
2012-12-20 12:44:37 +0900

    Add a test case for the prime-factorization function.

--- test/org/clojars/tnoda/math/prime_test.clj
+++ test/org/clojars/tnoda/math/prime_test.clj
@@ -6,6 +6,9 @@
   (testing "sieve"
     (is (= [2 3 5 7 11 13]
            (take 6 (sieve)))))
+  (testing "prime-factorization"
+    (is (= [2 2 3 3]
+           (prime-factorization 36))))
   (testing "prime-factors"
     (is (= {2 2 3 2}
            (prime-factors 36)))))
