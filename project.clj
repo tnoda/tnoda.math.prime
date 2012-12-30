48f5ef9a98463ccb7ab1a04de141ec4732223aa8
Takahiro Noda <takahiro.noda@gmail.com>
2012-12-30 21:26:55 +0900

    Amend the project name.
    That has not been updated since 0.1.0.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.tnoda/math.prime "0.2.0"
+(defproject org.clojars.tnoda/tnoda.math.prime "0.2.0"
   :description "A Clojure library that implements the sieve of Eratosthenes."
   :url "https://github.com/tnoda/tnoda.math.prime"
   :license {:name "Eclipse Public License"
